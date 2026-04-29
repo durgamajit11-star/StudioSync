6afb7b7b45592d122042b2ead206ba758a617b69
Ajit Durgam <durgamajit11@gmail.com>
2026-04-29 07:57:16 +0530

    Handle missing uploaded media gracefully

---
+++ studios/templatetags/media_extras.py
@@ -0,0 +1,21 @@
+from django import template
+from django.core.files.storage import default_storage
+
+
+register = template.Library()
+
+
+@register.filter
+def storage_exists(file_field):
+    if not file_field:
+        return False
+
+    try:
+        name = file_field.name
+    except ValueError:
+        return False
+
+    if not name:
+        return False
+
+    return default_storage.exists(name)
